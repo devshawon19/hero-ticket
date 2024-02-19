0ce1d45b873193004ca86468a300293f088c75d3
devshawon19 <srshawon1234@gmail.com>
2024-02-19 13:25:16 +0600

    validation left

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
-function goToTicket(){
+function goToTicket() {
     // targeted section to reach
     const ticketSection = document.getElementById('ticket-section');
 
     // scroll to the targeted section
-    ticketSection.scrollIntoView({behavior: 'smooth'})
+    ticketSection.scrollIntoView({ behavior: 'smooth' })
 
     /* this scrollIntoView was not included in programming hero course,
     I could've used the href attribute in anchor tag from the banner but
@@ -14,15 +14,30 @@ function goToTicket(){
     */
 }
 
+
 document.addEventListener('click', seatSelected);
 
-function seatSelected(event){
+
+
+
+
+// global variables
+const grandTotal = document.getElementById('grand-total');
+const discountTotal = document.getElementById('discount-total');
+let selectedSeats = 0;
+let totalMoney = 0;
+
+
+
+function seatSelected(event) {
     const clickedButton = event.target;
     // get the element you clicked
-    if(clickedButton.classList.contains('bus-seat') && !clickedButton.classList.contains('seat-selected') ) {
+    if (clickedButton.classList.contains('bus-seat') && !clickedButton.classList.contains('seat-selected')) {
+
+
         const seatNumber = clickedButton.innerText;
         // create a div to hold 3 info of each tickets
-        const  ticketDetailsHolder = document.createElement('div');
+        const ticketDetailsHolder = document.createElement('div');
         ticketDetailsHolder.classList.add('flex', 'justify-between');
 
 
@@ -43,11 +58,43 @@ function seatSelected(event){
         perTicketCost.innerText = 550;
         ticketDetailsHolder.appendChild(perTicketCost);
 
+
         // push the ticket info to the UI
         const wholeTicketDetails = document.getElementById('ticket-details');
         wholeTicketDetails.appendChild(ticketDetailsHolder);
-        
 
+        const seatCount = document.getElementById('selected-ticket');
+
+        // increase selected seats
+        selectedSeats++;
+        totalMoney = selectedSeats * 550;
+        const totalMoneyUi = document.getElementById('ticket-price-total');
+        totalMoneyUi.innerText = totalMoney;
+
+
+
+        // disable the buttons when it's limit reached and show alert
+        seatCount.innerText = selectedSeats;
+        if (selectedSeats === 4) {
+            const disableButtons = document.querySelectorAll('.bus-seat');
+            disableButtons.forEach(function (button) {
+                button.disabled = true;
+            })
+            const alert = document.getElementById('alert-ticket-limit');
+            alert.removeAttribute('hidden')
+
+        }
+
+        if (selectedSeats >= 4) {
+            const applyButton = document.getElementById('apply-button');
+            applyButton.disabled = false;
+        }
+
+        // update available seats after selecting tickets
+        const seatAvailable = document.getElementById('seat-available');
+        let availableSeats = parseInt(seatAvailable.innerText);
+        availableSeats = availableSeats - 1;
+        seatAvailable.innerText = availableSeats;
 
         // highlight the button clicked
         clickedButton.classList.remove('bg-[#f0f0f0]');
@@ -56,4 +103,26 @@ function seatSelected(event){
     }
 }
 
+function applyCoupon() {
+
+    const couponInput = document.getElementById('coupon-input');
+    const applyButton = document.getElementById('apply-button');
+    console.log(grandTotal.innerText)
+    if (couponInput.value === 'NEW15') {
+        let getDiscount = totalMoney * 0.15;
+        totalMoney = totalMoney - getDiscount;
+        discountTotal.innerText = getDiscount;
+        grandTotal.innerText = totalMoney;
+        couponInput.value = ''
+        applyButton.disabled = true;
+    }
+    if (couponInput.value === 'Couple 20') {
+        let getDiscount = totalMoney * 0.20;
+        totalMoney = totalMoney - getDiscount;
+        discountTotal.innerText = getDiscount;
+        grandTotal.innerText = totalMoney;
+        couponInput.value = ''
+        applyButton.disabled = true;
+    }
+}
 
